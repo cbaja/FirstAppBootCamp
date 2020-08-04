d075880c2bf1297eeaac64dd4f37cac630855de3
Carly BAJA <cbaja@vokeso.com>
2020-08-04 11:00:27 -0400

    directory setup

--- app/src/main/java/com/map/carlybaja/firstappbootcamp/activities/MainActivity.java
+++ app/src/main/java/com/map/carlybaja/firstappbootcamp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.map.carlybaja.firstappbootcamp;
+package com.map.carlybaja.firstappbootcamp.activities;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.map.carlybaja.firstappbootcamp.R;
 
 public class MainActivity extends AppCompatActivity {
 
